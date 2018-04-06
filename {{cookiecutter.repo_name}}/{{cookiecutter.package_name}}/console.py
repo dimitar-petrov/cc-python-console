dc93465761f1e5c038d1a3963baace5b0f44bbf1
Dimitar Petrov <petrov.dimp@gmail.com>
2018-04-06 22:45:58 +0300

    Add logging configuration

--- {{cookiecutter.repo_name}}/{{cookiecutter.package_name}}/console.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.package_name}}/console.py
@@ -1,8 +1,33 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals, absolute_import, print_function
+import os
+import yaml
 import click
+import logging.config
+
+
+def setup_logging(default_path='logging.yml',
+                  default_level=logging.INFO,
+                  env_key='LOG_CFG'):
+    """Setup logging configuration."""
+
+    path = default_path
+    value = os.getenv(env_key, None)
+    if value:
+        path = value
+    if os.path.exists(path):
+        with open(path, 'rt') as f:
+            config = yaml.load(f)
+        logging.config.dictConfig(config)
+    else:
+        logging.basicConfig(level=default_level)
 
 
 @click.command()
 def main():
     print('I am {{cookiecutter.console_app_name}}')
+
+
+if __name__ == "__main__":
+    setup_logging()
+    main()
